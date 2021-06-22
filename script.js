bd3bdd1ba391c9d34e6281ac6dd76206755965b4
wombassador <62153486+wombassador@users.noreply.github.com>
2021-06-22 11:55:34 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,43 @@
+$(".caroline").mouseenter(function(){
+$(".caroline").css("background","navy");
+    $(".ife-name").css("color","white");
+});
+
+$(".caroline").mouseleave(function(){
+$(".caroline").css("background","transparent");
+    $(".ife-name").css("color","blue");
+});
+
+$(".raj").mouseenter(function(){
+$(".raj").css("background","yellow");
+});
+
+$(".raj").mouseleave(function(){
+$(".raj").css("background","transparent");
+});
+
+$(".jerry").mouseenter(function(){
+$(".jerry").css("background","red");
+    $(".adam-name").css("color","white");
+});
+
+$(".jerry").mouseleave(function(){
+$(".jerry").css("background","transparent");
+    $(".adam-name").css("color","blue");
+});
+
+$(".jason").mouseenter(function(){
+$(".jason").css("background","beige");
+});
+
+$(".jason").mouseleave(function(){
+$(".jason").css("background","transparent");
+});
+
+$(".kimberly").mouseenter(function(){
+$(".kimberly").css("background","aqua");
+});
+
+$(".kimberly").mouseleave(function(){
+$(".kimberly").css("background","transparent");
+});
